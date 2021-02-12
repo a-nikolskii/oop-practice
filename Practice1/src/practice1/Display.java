e247246bb6d19bce30619572b9c37b8154b179be
a-nikolskii <64317800+a-nikolskii@users.noreply.github.com>
2021-02-12 16:19:11 +0300

    Add files via upload

---
+++ Practice1/src/practice1/Display.java
@@ -0,0 +1,35 @@
+package practice1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author a.nikolskii
+ */
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Display extends JPanel {
+    private JTextField display;
+
+    public Display() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+        this.setLayout(new GridLayout(1, 1));
+
+        display = new JTextField("0");
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        this.add(display);
+    }
+
+    public void setValue(String value) {
+        this.display.setText(value);
+    } 
+}
